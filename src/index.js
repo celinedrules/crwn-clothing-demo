c0e58f6b1e01771a37c715ad28e7b85061f55cc7
celinedrules <celinedrules@hotmail.com>
2021-08-04 14:50:59 -0600

    Updating

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+import reportWebVitals from './reportWebVitals';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/redux/root-saga.js
@@ -0,0 +1,13 @@
+﻿import { all, call } from 'redux-saga/effects';
+
+export default function* rootSaga()
+{
+	
+}
+// import { shopSagas } from './shop/shop.sagas';
+// import { userSagas } from './user/user.sagas';
+// import { cartSagas } from './cart/cart.sagas';
+//
+// export default function* rootSaga() {
+// 	yield all([call(shopSagas), call(userSagas), call(cartSagas)]);
+//}
