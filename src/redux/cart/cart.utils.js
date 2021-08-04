6883d58d4a6b95d3d71db5eeabf8c8c27ffaff3f
celinedrules <celinedrules@hotmail.com>
2021-08-04 15:59:33 -0600

    Added redux

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,64 @@
+﻿import { useEffect, useRef, useState } from 'react';
+import { toggleCartHidden } from './cart.actions';
+
+export const addItemToCart = (cartItems, cartItemToAdd) =>
+{
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToAdd.id,
+	);
+
+	if (existingCartItem)
+	{
+		return cartItems.map(cartItem =>
+			cartItem.id === cartItemToAdd.id
+				? { ...cartItem, quantity: cartItem.quantity + 1 }
+				: cartItem,
+		);
+	}
+
+	return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) =>
+{
+	const existingCartItem = cartItems.find(
+		cartItem => cartItem.id === cartItemToRemove.id,
+	);
+
+	if (existingCartItem.quantity > 1)
+	{
+		return cartItems.map(cartItem =>
+			cartItem.id === cartItemToRemove.id
+				? { ...cartItem, quantity: cartItem.quantity - 1 }
+				: cartItem,
+		);
+	}
+
+	return cartItems;
+};
+
+export const ClickedOutside = () =>
+{
+	const wrapperRef = useRef(null);
+	const [isVisible, setIsVisible] = useState(true);
+
+	// below is the same as componentDidMount and componentDidUnmount
+	useEffect(() =>
+	{
+		document.addEventListener('click', handleClickOutside, false);
+		return () =>
+		{
+			document.removeEventListener('click', handleClickOutside, false);
+		};
+	}, []);
+
+	const handleClickOutside = event =>
+	{
+		if (wrapperRef.current && !wrapperRef.current.contains(event.target))
+		{
+			setIsVisible(false);
+			toggleCartHidden();
+		}
+	};
+	return { wrapperRef, isVisible };
+};
---
+++ src/redux/shop/shop.actions.js
@@ -0,0 +1,16 @@
+﻿import ShopActionTypes from './shop.types';
+
+// Don't know that I need this as it's in shop.sagas.js
+export const fetchCollectionsStart = collectionsMap => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+	payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+	type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+	payload: errorMessage,
+});
