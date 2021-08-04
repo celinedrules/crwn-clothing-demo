608eb632ae54a7c7857da60f3cb41475243ef2a9
celinedrules <celinedrules@hotmail.com>
2021-08-04 16:13:38 -0600

    Added additional pages

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { createStructuredSelector } from 'reselect';
 import Header from './components/header/header.component';
-// import HomePage from './pages/homepage/homepage.component';
-// import ShopPage from './pages/shop/shop.component';
-// import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-// import CheckoutPage from './pages/checkout/checkout.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 //import { selectCurrentUser } from './redux/user/user.selectors';
 //import { checkUserSession } from './redux/user/user.actions';
 
@@ -20,22 +20,22 @@ const App = ({ /*checkUserSession, currentUser*/ }) => {
     return (
         <div>
             <Header />
-            {/*<Switch>*/}
-            {/*    <Route exact path={'/'} component={HomePage} />*/}
-            {/*    <Route path={'/shop'} component={ShopPage} />*/}
-            {/*    <Route exact path={'/checkout'} component={CheckoutPage} />*/}
-            {/*    <Route*/}
-            {/*        exact*/}
-            {/*        path={'/signin'}*/}
-            {/*        render={() =>*/}
-            {/*            currentUser ? (*/}
-            {/*                <Redirect to={'/'} />*/}
-            {/*            ) : (*/}
-            {/*                <SignInAndSignUpPage />*/}
-            {/*            )*/}
-            {/*        }*/}
-            {/*    />*/}
-            {/*</Switch>*/}
+            <Switch>
+                <Route exact path={'/'} component={HomePage} />
+                <Route path={'/shop'} component={ShopPage} />
+                <Route exact path={'/checkout'} component={CheckoutPage} />
+                {/*<Route*/}
+                {/*    exact*/}
+                {/*    path={'/signin'}*/}
+                {/*    render={() =>*/}
+                {/*        currentUser ? (*/}
+                {/*            <Redirect to={'/'} />*/}
+                {/*        ) : (*/}
+                {/*            <SignInAndSignUpPage />*/}
+                {/*        )*/}
+                {/*    }*/}
+                {/*/>*/}
+            </Switch>
         </div>
     );
 };
