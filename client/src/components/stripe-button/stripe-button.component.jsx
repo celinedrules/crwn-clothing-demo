11b0f2c70df8e5d20ce1e597644bb702baf5c9e3
celinedrules <celinedrules@hotmail.com>
2021-08-05 17:10:35 -0600

    Update stripe-button.component.jsx

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,56 +1,49 @@
 ﻿import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
 	const priceForStripe = price * 100;
 	const publishableKey =
 		'pk_test_51JH6hyD5a5nqvcruNAs1UMI7S1gKyfRXIgfELdtSKVWY1KqbVLxODdATHHlMOAgNgXcKrWFVVkK56AP2HWJmlKnf00H6vPTHnL';
 
-
-	const onToken = token => {
-
-		const url = 'checkout';
-
-		const options = {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json;charset=UTF-8',
-			},
-			body: JSON.stringify({
-				amount: priceForStripe,
-				token,
-			}),
-		};
-		fetch(url, options)
+	const makePayment = token => {
+		const body = {
+			token,
+			priceForStripe
+		}
+		const headers = {
+			"Content-Type": "application/json"
+		}
+		return fetch('checkout', {
+			method: "POST",
+			headers,
+			body: JSON.stringify(body)
+		})
 			.then(response => {
-				alert('Payment Successful');
-			}).catch(error => {
-			console.log('Payment error:', error);
-			alert('There was in issue with your payment. Please make sure you use the provided credit card.');
-		});
-		// axios(options)
-		// 	.then(response => {
-		// 		alert('Payment Successful');
-		// 	}).catch(error => {
-		// 	console.log('Payment error:', error);
-		// 	alert('There was in issue with your payment. Please make sure you use the provided credit card.');
-		// });
+				console.log("RESPONSE : ",response)
+				const {status} = response
+				console.log("status : ",status)
+			})
+			.catch(err => {
+				console.log(err)
+			})
 	};
+	
 	// const onToken = token => {
-	//   axios({
-	//     url: "checkout",
-	//     method: "post",
-	//     data: {
-	//       amount: priceForStripe,
-	//       token
-	//     }
-	//   }).then(response => {
-	//     alert("Payment Successful");
-	//   }).catch(error => {
-	//     console.log("Payment error:", error);
-	//     alert("There was in issue with your payment. Please make sure you use the provided credit card.");
-	//   });
+	// 	axios({
+	// 		url: 'checkout',
+	// 		method: 'post',
+	// 		data: {
+	// 			amount: priceForStripe,
+	// 			token,
+	// 		},
+	// 	}).then(response => {
+	// 		alert('Payment Successful');
+	// 	}).catch(error => {
+	// 		console.log('Payment error:', error);
+	// 		alert('There was in issue with your payment. Please make sure you use the provided credit card.');
+	// 	});
 	// };
 
 	return (
@@ -63,7 +56,7 @@ const StripeCheckoutButton = ({ price }) => {
 			description={`Your total is $${price}`}
 			amount={priceForStripe}
 			panelLabel={'Pay Now'}
-			token={onToken}
+			token={makePayment}
 			stripeKey={publishableKey}
 		/>
 	);
