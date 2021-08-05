abadc8b109900c7baf1eb6f6b48e38b04973f6ef
celinedrules <celinedrules@hotmail.com>
2021-08-05 05:58:42 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
-//import { selectCurrentUser } from './redux/user/user.selectors';
-//import { checkUserSession } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 import './App.css';
 
-const App = ({ /*checkUserSession, currentUser*/ }) => {
-    // useEffect(() => {
-    //     checkUserSession();
-    // }, [checkUserSession]);
+const App = ({ checkUserSession, currentUser }) => {
+    useEffect(() => {
+        checkUserSession();
+    }, [checkUserSession]);
 
     return (
         <div>
@@ -40,13 +40,12 @@ const App = ({ /*checkUserSession, currentUser*/ }) => {
     );
 };
 
-// const mapStateToProps = createStructuredSelector({
-//     currentUser: selectCurrentUser,
-// });
-//
-// const mapDispatchToProps = dispatch => ({
-//     checkUserSession: () => dispatch(checkUserSession()),
-// });
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(App);
-export default App;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+});
+
+const mapDispatchToProps = dispatch => ({
+    checkUserSession: () => dispatch(checkUserSession()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
