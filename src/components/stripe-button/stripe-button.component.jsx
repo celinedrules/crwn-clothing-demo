adea394924c7918a7d1ae20b49896c5f0aec5c90
celinedrules <celinedrules@hotmail.com>
2021-08-07 16:09:09 -0600

    Removed client directory
    Moved everything from client folder to root

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,48 @@
+﻿import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+	const priceForStripe = price * 100;
+	console.log(price);
+	const publishableKey =
+		'pk_test_51JH6hyD5a5nqvcruNAs1UMI7S1gKyfRXIgfELdtSKVWY1KqbVLxODdATHHlMOAgNgXcKrWFVVkK56AP2HWJmlKnf00H6vPTHnL';
+
+	const onToken = token => {
+		const url = '/.netlify/functions/charge-card';
+		const options = {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json;charset=UTF-8',
+			},
+			body: JSON.stringify({
+				amount: priceForStripe,
+				token,
+			}),
+		};
+		fetch(url, options)
+			.then(response => {
+				alert('Payment Successful');
+			}).catch(error => {
+			console.log('Payment error:', error);
+			alert('There was in issue with your payment. Please make sure you use the provided credit card.');
+		});
+	};
+
+	return (
+		<StripeCheckout
+			label={'Pay Now'}
+			name={'CRWN Clothing Ltd.'}
+			billingAddress
+			shippingAddress
+			image={'https://svgshare.com/i/CUz.svg'}
+			description={`Your total is $${price}`}
+			amount={priceForStripe}
+			panelLabel={'Pay Now'}
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
+};
+
+export default StripeCheckoutButton;
---
+++ src/functions/charge-card.js
@@ -0,0 +1,27 @@
+﻿require('encoding');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event, context) => {
+
+	const data = JSON.parse(event.body);
+	const body = {
+		source: data.token.id,
+		amount: data.amount,
+		currency: 'usd',
+	};
+
+	await stripe.charges.create(body, (stripeErr, stripeRes) => {
+		if (stripeErr) {
+			return {
+				statusCode: 500,
+				body: stripeErr,
+			};
+		} else {
+			return {
+				statusCode: 200,
+				body: stripeRes,
+			};
+		}
+	});
+};
