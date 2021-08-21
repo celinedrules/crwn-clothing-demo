a9bf7482f380af32f66aa3fc0a7f572cc8561a32
celinedrules <celinedrules@hotmail.com>
2021-08-21 11:25:37 -0600

    updated header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { ReactComponent as Logo } from '../../assets/thanyou.svg';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import CartIcon from '../cart-icon/cart-icon.component';
 import * as sc from './header.styles';
 import CartDropdownContainer from '../cart-dropdown/cart-dropdown.container';
 import UserMenu from '../user-menu/user-menu.component';
+import { Title } from './header.styles';
 
 const Header = ({ hidden }) => {
 	return (
@@ -15,6 +16,9 @@ const Header = ({ hidden }) => {
 			<sc.LogoContainer to={'/'}>
 				<Logo className={'logo'} />
 			</sc.LogoContainer>
+			<Title>
+				Xiè Xiè Beads
+			</Title>
 			<sc.OptionsContainer style={{ paddingRight: '80px' }}>
 				<sc.OptionLink to={'/shop'}>SHOP</sc.OptionLink>
 				<sc.OptionLink to={'/contact'}>CONTACT</sc.OptionLink>
--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 export const HeaderContainer = styled.div`
 	height: 70px;
 	width: 100%;
-	display: flex;
-	justify-content: space-between;
+	display: flex; 
+	//justify-content: space-between;
 	margin-bottom: 25px;
 `;
 
 export const LogoContainer = styled(Link)`
 	height: 100%;
 	width: 70px;
-	padding: 25px;
+  padding: 10px;
 `;
 
 export const OptionsContainer = styled.div`
@@ -24,8 +24,21 @@ export const OptionsContainer = styled.div`
 `;
 
 export const OptionLink = styled(Link)`
-	padding: 10px 0px 0px 20px;
+	padding: 10px 0 0 20px;
 	cursor: pointer;
+`;
+
+export const Title = styled.div`
+  width: 100%;
+  font-family: 'Dancing Script', cursive;
+  font-size: 72px;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  padding-left: 250px;
+  padding-top: 25px;
+  
 `;
 
 HeaderContainer.displayName = 'HeaderContainer';
